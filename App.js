8b06405c59ca69aeb73d4e23e3aa943587ad4810
Shreya-1206 <shreyaray.1206@gmail.com>
2025-03-26 12:17:17 +0530

    jsx and react element

--- App.js
+++ App.js
@@ -2,36 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 
 
-// Now How make nested element in react 
-{/* <div id= "parent">
-    <div class = "child1">
-        <h1>AM h1 Tag </h1>
-        <h2>AM h2 Tag </h2>
-    </div>
-    <div class = "child2">
-        <h1>AM h1 Tag </h1>
-        <h2>AM h2 Tag </h2>
-    </div>
-</div> */}
 
-const parent = React.createElement("div" ,{id:"parent"}, 
-    [
-        React.createElement("div", {id:"child1",  key: "child1"}, 
-            [React.createElement('h1', { key: "h1-child1"}, "Hey am H1 from child1"),
-                 React.createElement('h2', { key: "h2-child1"}, "Hey am H2 from child1")]),
-        React.createElement("div", {id:"child2",  key: "child2"}, 
-            [React.createElement('h1', { key: "h1-child2"}, "Hey am H1 from child2"),
-                React.createElement('h2', { key: "h2-child2"}, "Hey am H2 from child2")])
-    ])
-
-const heading = React.createElement("h1", {
-    id:"heading",
-    xyz : "abcde"
-}, "Hello World From React🚀")
-
-console.log(heading) // Its AN react Element (Object)
+// React.createElement => converts in react element (Object) => Html element (on reneder)
+const heading = React.createElement('h1', {id :"heading"}, 'Hello, world!')
 
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// JSX =>To React.createElement by babel => converts in react element (Object) => Html element (on reneder)       /// multiline jsx
+const jsxHeading =( <h1 
+    id = "heading" 
+    className='head' 
+    tabIndex={3}>
+        Hello, world 🤩</h1> )
 
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
