31e644b3c804192c4c186a6513b3c5c4df71b100
Shreya-1206 <shreyaray.1206@gmail.com>
2025-03-27 14:45:47 +0530

    Now we have used an swiggy obj to inject dynamic data to resCard

--- App.js
+++ App.js
@@ -17,7 +17,113 @@ import ReactDOM from 'react-dom/client'
 //   - Address Component
 //   - Links  Component 
 
-
+const resObj = {
+    "@type": "type.googleapis.com/swiggy.presentation.food.v2.Restaurant",
+    "info": {
+      "id": "47439",
+      "name": "Leon's - Burgers & Wings (Leon Grill)",
+      "cloudinaryImageId": "RX_THUMBNAIL/IMAGES/VENDOR/2024/11/14/119c66fd-2da6-476f-a6cf-eb3a997444c2_47439.jpg",
+      "locality": "Outer Ring Road",
+      "areaName": "Marathahalli",
+      "costForTwo": "₹400 for two",
+      "cuisines": [
+        "American",
+        "Snacks",
+        "Turkish",
+        "Portuguese",
+        "Continental"
+      ],
+      "avgRating": 4.3,
+      "parentId": "371281",
+      "avgRatingString": "4.3",
+      "totalRatingsString": "62K+",
+      "sla": {
+        "deliveryTime": 37,
+        "lastMileTravel": 5.6,
+        "serviceability": "SERVICEABLE",
+        "slaString": "35-40 mins",
+        "lastMileTravelString": "5.6 km",
+        "iconType": "ICON_TYPE_EMPTY"
+      },
+      "availability": {
+        "nextCloseTime": "2025-03-28 04:00:00",
+        "opened": true
+      },
+      "badges": {
+        
+      },
+      "isOpen": true,
+      "type": "F",
+      "badgesV2": {
+        "entityBadges": {
+          "imageBased": {
+            
+          },
+          "textBased": {
+            
+          },
+          "textExtendedBadges": {
+            
+          }
+        }
+      },
+      "aggregatedDiscountInfoV3": {
+        "header": "ITEMS",
+        "subHeader": "AT ₹89"
+      },
+      "orderabilityCommunication": {
+        "title": {
+          
+        },
+        "subTitle": {
+          
+        },
+        "message": {
+          
+        },
+        "customIcon": {
+          
+        }
+      },
+      "differentiatedUi": {
+        "displayType": "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        "differentiatedUiMediaDetails": {
+          "mediaType": "ADS_MEDIA_ENUM_IMAGE",
+          "lottie": {
+            
+          },
+          "video": {
+            
+          }
+        }
+      },
+      "reviewsSummary": {
+        
+      },
+      "displayType": "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      "restaurantOfferPresentationInfo": {
+        
+      },
+      "externalRatings": {
+        "aggregatedRating": {
+          "rating": "4.8",
+          "ratingCount": "818"
+        },
+        "source": "GOOGLE",
+        "sourceIconImageId": "v1704440323/google_ratings/rating_google_tag"
+      },
+      "ratingsDisplayPreference": "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY"
+    },
+    "analytics": {
+      "context": "seo-data-3861d43d-6b35-4ea5-aec7-7b877307ce4d"
+    },
+    "cta": {
+      "link": "https://www.swiggy.com/city/bangalore/leons-burgers-and-wings-leon-grill-outer-ring-road-marathahalli-rest47439",
+      "text": "RESTAURANT_MENU",
+      "type": "WEBLINK"
+    },
+    "widgetId": "collectionV5RestaurantListWidget_SimRestoRelevance_food_seo"
+  };
 const Header = () => {
     return (
         <div className= "header">
@@ -47,30 +153,45 @@ const Body = () => {
              <button>Search</button>
           </div>
           <div className = "res-container">
-            <RestuarantCard 
-            resName ="Meghna Foods" 
-            resPrice = "Rs. 500" 
-            resRating = "4.5" 
-            resCuisine = {["Briyani", "North Indian"]}
-            resDeliveryTime ="30-40 mins"/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/> <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+            <RestuarantCard resData = {resObj}/>
+
           </div>
         </div>
     )
 }
 
 const RestuarantCard = (props) => {
-    console.log(props);
-    const {resName, resPrice, resRating, resCuisine, resDeliveryTime} = props;
+    const {resData} = props;
+    console.log(resData);
     return (
         <div className = "res-card">
             <img className = "res-image" src = "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/e0vvulfbahjxjz6k4uwi"/>
-            <h3 className = "res-name">{resName}</h3>
-            <h4 className = "res-cuisine">{resCuisine.join(", ")}</h4>
+            <h3 className = "res-name">{resData.info.name}</h3>
+            <p className = "res-cuisine">{resData.info.cuisines.join(", ")}</p>
             <div className = "rating-del-conatainer">
-             <h4 className = "res-rating">⭐ {resRating}</h4>
-             <h4 className = "res-deliveryTime">{resDeliveryTime}</h4>
+             <h4 className = "res-rating">⭐ {resData.info.avgRating}</h4>
+             <h4 className = "res-deliveryTime">{resData.info.sla.deliveryTime} mintues</h4>
             </div>
-            <h4 className = "res-place">Central Bangalore</h4>
+            <h4 className = "res-place">{resData.info.areaName}</h4>
         </div>
     )
 }
