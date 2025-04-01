c1c2998e4ec58c859dd72f0f8150274418e815ec
Shreya-1206 <shreyaray.1206@gmail.com>
2025-04-01 13:30:14 +0530

    Updated the ui a bit for search and filter and onClick top restuarants loaded using normal js varible but ui didnt updated

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,15 +2,43 @@ import RestuarantCard from "./RestuarantCard";
 import {resList} from "../utlis/mockData";
 
 const Body = () => {
+   //Normal js variable
+   let restaurantList = resList;
     return(
         <div className = "body">
-          <div className = "search-container">
-             <input/>
-             <button>Search</button>
+          <div className = "search-filter-container">
+             <div className ="search">
+              <input/>
+              <button>Search</button>
+             </div>
+             <div className = "filters">
+               <ul className="list-of-filters">
+                  <li>
+                   <button
+                      onClick={() => {
+                        restaurantList = restaurantList.filter(res => res.info.avgRating > 4.1);
+                        console.log(restaurantList)
+                      }}
+                    >Top Rated Restuarants</button>
+                  </li>
+                  <li>
+                   <button>Fast Delivery</button>
+                  </li>
+                  <li>
+                   <button>New On Board</button>
+                  </li>
+                  <li>
+                   <button>Offers</button>
+                  </li>
+                  <li>
+                   <button>Less than Rs300</button>
+                  </li>
+               </ul>
+             </div>
           </div>
           <div className = "res-container">
-             {resList.map((restuarant) => (
-                <RestuarantCard 
+             {restaurantList.map((restuarant) => (
+                <RestuarantCard
                 resData = {restuarant} 
                 key = {restuarant.info.id} />
              ))}
